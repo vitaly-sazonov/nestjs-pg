e503ace9d9ec4d6aece95abc80a0d1c5c1d82e98
vitomax <v.o.sazonov@gmail.com>
2022-07-21 16:20:52 +0300

    feat: remove module PgCore

--- src/pg.core.ts
+++ src/pg.core.ts
@@ -1,38 +1,13 @@
-import { DynamicModule, Global, Inject, Module } from "@nestjs/common";
-import { ModuleRef } from "@nestjs/core";
-import { Pool } from "pg";
-
-import { PG_MODULE_OPTIONS } from "./pg.constants";
+import { DynamicModule, Module } from "@nestjs/common";
+import { PgCoreModule } from "./pg.core";
 import { IPgModuleOptions } from "./pg.interfaces";
-import { getPoolToken } from "./pg.utils";
 
-@Global()
 @Module({})
-export class PgCoreModule {
-  constructor(
-    @Inject(PG_MODULE_OPTIONS)
-    private readonly options: IPgModuleOptions,
-    private readonly moduleRef: ModuleRef
-  ) {}
-
+export class PgModule {
   static forRoot(options: IPgModuleOptions): DynamicModule {
-    const pgOptions = {
-      provide: PG_MODULE_OPTIONS,
-      useValue: options,
-    };
-    const poolProvider = {
-      provide: getPoolToken(options),
-      useFactory: async () => this.createPoolFactory(options),
-    };
-
     return {
-      module: PgCoreModule,
-      providers: [poolProvider, pgOptions],
-      exports: [poolProvider],
+      module: PgModule,
+      imports: [PgCoreModule.forRoot(options)],
     };
-  }
-
-  private static createPoolFactory(options) {
-    return new Pool(options);
   }
 }
--- src/pg.module.ts
+++ src/pg.module.ts
@@ -1,13 +1,38 @@
-import { DynamicModule, Module } from "@nestjs/common";
-import { PgCoreModule } from "./pg.core";
-import { IPgModuleOptions } from "./pg.interfaces";
+import { DynamicModule, Global, Inject, Module } from "@nestjs/common";
+import { ModuleRef } from "@nestjs/core";
+import { Pool } from "pg";
 
+import { PG_MODULE_OPTIONS } from "./pg.constants";
+import { IPgModuleOptions } from "./pg.interfaces";
+import { getPoolToken } from "./pg.utils";
+
+@Global()
 @Module({})
 export class PgModule {
+  constructor(
+    @Inject(PG_MODULE_OPTIONS)
+    private readonly options: IPgModuleOptions,
+    private readonly moduleRef: ModuleRef
+  ) {}
+
   static forRoot(options: IPgModuleOptions): DynamicModule {
+    const pgOptions = {
+      provide: PG_MODULE_OPTIONS,
+      useValue: options,
+    };
+    const poolProvider = {
+      provide: getPoolToken(options),
+      useFactory: async () => this.createPoolFactory(options),
+    };
+
     return {
       module: PgModule,
-      imports: [PgCoreModule.forRoot(options)],
+      providers: [poolProvider, pgOptions],
+      exports: [poolProvider],
     };
+  }
+
+  private static createPoolFactory(options) {
+    return new Pool(options);
   }
 }
